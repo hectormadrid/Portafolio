b6ceb4c0311616f5dd23907aab89d98dde9bdd29
Héctor Madrid <74695862+hectormadrid@users.noreply.github.com>
2025-08-13 11:42:16 -0400

    archivos js arreglados

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-import './scrollAnimations.js';
-import './progressBars.js';
-import './typingEffect.js';
-import './themeToggle.js';
-import './contactForm.js';
+import {initScrollAnimations} from './scrollAnimations.js';
+import {initProgressBars}from './progessBar.js';
+import {initcontactFrom} from'./contactForm.js';
+import {initThemeToggle}from './themeToggles.js';
+import {initTypingEffect}from './typingEffect.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    initScrollAnimations();
+    initProgressBars();
+    initThemeToggle();
+    initTypingEffect();
+    initcontactFrom();
+});
